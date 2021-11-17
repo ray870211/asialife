1c91b07cc5b17fcd223fd4dc0591e74cd4a20883
ray <ray870211@gmail.com>
2021-11-17 16:15:28 +0800

    I

--- asialife/LoginController.swift
+++ asialife/LoginController.swift
@@ -14,22 +14,21 @@ class LoginController:UIViewController{
     @IBOutlet weak var loginBtnContainerView1: UIView!
     @IBOutlet weak var loginBtnContainerView2: UIView!
     @IBOutlet weak var fbBtn: UIButton!
-    var account = String()
+    var email = String()
     var password = String()
+    var responseData:[String] = []
     static let toLoginButtonVC1 = "loginButtonVC1"
     static let toLoginButtonVC2 = "loginButtonVC2"
-//    let viewController = ViewController()
+    //    let viewController = ViewController()
     let loginButtonVC2 =  LoginButtonVC2()
     //    var isOutSide: Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Do any additional setup after loading the view.
         loginBtnContainerView2.isHidden=true
         fbBtn.contentVerticalAlignment = .fill
         fbBtn.contentHorizontalAlignment = .fill
-        //        getLoginData()
     }
     
     
@@ -45,22 +44,70 @@ class LoginController:UIViewController{
                 
             }
         }
+        
     }
     
     @IBAction func login(_ sender: UIButton) {
-//        loginButtonVC2.setText()
-//        print(loginButtonVC2.accountTextField.text ?? "no")
-        print(TextFieldModel.sharedModel.accountText ?? "")
-        account = TextFieldModel.sharedModel.accountText ?? ""
-        if(account=="123"){
-            performSegue(withIdentifier: "ToIndexPageSegue", sender: sender)
-        }
-        
+        email = "ray870211@gmail.com"
+        password = "12345"
+        postApi()
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //鍵盤
         self.view.endEditing(true)
     }
-   
+    
+    func getApi(email:String , password:String){
+        let url = URL(string: "http://172.20.10.3/search.php?email=\(email)&password=\(password)")!
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data,
+               let content = String(data: data, encoding: .utf8) {
+                print(content)
+                
+            }
+        }.resume()
+    }
+    func postApi(){
+        struct CreateUserResponse: Decodable {
+            let status_code: String
+            let message: String
+        }
+        let postData = "email=\(email)&password=\(password)".data(using: .utf8)
+        let url = URL(string: "http://192.168.1.19//login.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = postData
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    //                    print(String(data: data,encoding: .utf8))
+                    let decoder = JSONDecoder()
+                    let createUserResponse = try decoder.decode(CreateUserResponse.self, from: data)
+                    if(createUserResponse.status_code=="200"){
+                        DispatchQueue.main.async {
+                            //切換到主線程
+                            // UIView usage
+                            self.performSegue(withIdentifier: "ToIndexPageSegue", sender: self)
+                        }
+                    }
+                    if(createUserResponse.status_code=="404"){
+                        DispatchQueue.main.async {
+                            //切換到主線程b
+                            let controller = UIAlertController(title: "錯誤", message: "帳號或密碼錯誤", preferredStyle: .alert)
+                            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            controller.addAction(okAction)
+                            self.present(controller, animated: true, completion: nil)
+                        }
+                    }
+                    print(createUserResponse)
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
 }
 
 
--- asialife/ViewController.swift
+++ asialife/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        containerView2.isHidden=true
-//        self.hidesBottomBarWhenPushed = false
+        postApi()
     }
     
     @IBAction func changContainerBtn(_ sender: UISegmentedControl) {
@@ -24,11 +22,39 @@ class ViewController: UIViewController {
         if(containerView1.isHidden==false){
             containerView1.isHidden=true
             containerView2.isHidden=false
+            
         }else{
             containerView1.isHidden=false
             containerView2.isHidden=true
         }
     }
-    
+    func postApi(){
+        struct StoreData: Decodable{
+            let store_data : [CreateUserResponse]
+        }
+        struct CreateUserResponse: Decodable {
+            let id: String
+            let name: String
+            let image: String
+        }
+        let url = URL(string: "http://192.168.1.19//select_store.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    //                    print(String(data: data,encoding: .utf8))
+                    let decoder = JSONDecoder()
+                    let createUserResponse = try decoder.decode(StoreData.self, from: data)
+                
+                    print(createUserResponse.store_data[1])
+                    
+                } catch  {
+                    print(error)
+                }
+            }
+        }.resume()
+    }
 }
 
